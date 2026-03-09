002d224dcc748cd12b5449f0b4d4aaaa3cde2918
Rodrigo Grego <rodrigo.grego@dortech-group.com>
2026-03-09 16:09:22 -0300

    Create Task Model

--- src/main/java/com/project/learning/models/User.java
+++ src/main/java/com/project/learning/models/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,6 +14,8 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 // Indica que esta classe é uma entidade JPA, o que significa que ela está pronta para ser
@@ -54,7 +57,8 @@ public class User {
 
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
 
@@ -92,6 +96,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 
     @Override
     public boolean equals(Object obj) {
