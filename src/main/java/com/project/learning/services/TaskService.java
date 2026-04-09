e2ce1aed0fa887e16a7bb3e55938eb5acbd69feb
Rodrigo Grego <rodrigogrego18@gmail.com>
2026-04-09 11:13:53 -0300

    Retorno da lista de tarefas de acordo com o usuário

--- src/main/java/com/project/learning/services/TaskService.java
+++ src/main/java/com/project/learning/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.project.learning.services;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -25,6 +26,11 @@ public class TaskService {
         return task.orElseThrow(() -> new RuntimeException(
              "Tarefa não encontada! Id: " + id + ", Tipo: "+ Task.class.getName()
         ));
+    }
+
+    public List<Task> findAllByUserId(Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
